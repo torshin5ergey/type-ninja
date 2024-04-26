9666b72149da78f1df870f7012ff082fc7abd5df
TestName <testEmail@example.com>
2024-04-26 14:30:27 +0300

    Cleaned code

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-import curses
 import time
 import threading
+import curses
 import ui
 import text_generator
 
-def get_user_input(stdscr, user_input:str):
-    #global typed, to_type
+KEY_EXIT = 24 # ^X exit
+KEY_START = 18 # ^R start
+KEY_BACKSPACE_UNX = 127 # backspace
+KEY_BACKSPACE_WND = 8 # backspace
+KEY_SPACE = 32 # space
+
+def get_user_input(stdscr, user_input:str, user_typed:list[str], task:list[str], task_typed:list[str]) -> tuple:
     curses.cbreak()
     char = stdscr.getch()
-    if char == curses.KEY_BACKSPACE or char == 127 or char == 8: # backspace
-        if len(user_input) > 0:
+    if (char == curses.KEY_BACKSPACE or char == KEY_BACKSPACE_UNX or char == KEY_BACKSPACE_WND):
+        if len(user_input) == 0:
+            user_input = ''
+        else:
             user_input = user_input[:-1]
-    elif char == 32: # space
+    elif char == KEY_SPACE:
         user_typed.append(user_input)
-        task_typed.append(text.pop(0))
-        return ''
+        task_typed.append(task.pop(0))
+        user_input = ''
     else:
         user_input += chr(char)
-    return user_input
+    return user_input, task, task_typed
 
 def is_correct_input(user_input:str, task) -> bool:
     if user_input == task[:len(user_input)]:
@@ -25,7 +32,7 @@ def is_correct_input(user_input:str, task) -> bool:
     else:
         return False
 
-def time_counter(stdscr, start:float):
+def time_counter(stdscr, start:float) -> None:
     global remaining
     while True:
         remaining = round(timer - (time.time() - start))
@@ -34,13 +41,12 @@ def time_counter(stdscr, start:float):
         ui.display_time(stdscr, remaining)
         time.sleep(.01)
 
-def calculate_stats(typed_words:list[str], task:list[str], remaining:int):
+def calculate_stats(typed_words:list[str], task:list[str], remaining:int) -> tuple:
     # User typed words stats
     typed_words_count = len(typed_words)
     typed_chars = len(" ".join(typed_words))
     # Task words stats
     task_chars = len(" ".join(task))
-
     # Calculate WPM (words per minute)
     try:
         wpm = round(typed_words_count / ((timer - remaining) / 60))
@@ -65,21 +71,17 @@ def calculate_stats(typed_words:list[str], task:list[str], remaining:int):
         incorrect_chars = max(len(w1), len(w2)) - correct_chars
         total_correct_chars += correct_chars
         total_incorrect_chars += incorrect_chars
-
     total_chars = sum(max(len(w1), len(w2)) for w1, w2 in zip(task, typed_words))
     try:
         char_accuracy = round((total_correct_chars / total_chars) * 100, 2)
     except ZeroDivisionError:
         char_accuracy = 0
-    
     return wpm, char_accuracy, total_incorrect_chars, words_accuracy, words_incorrect
 
 def main(stdscr):
-    global user_typed, text, is_correct, task_typed
     global timer, remaining
     # Main menu loop
     while True:
-            # curses.curs_set(0)
             stdscr.clear()
             curses.curs_set(False)
             curses.noecho()
@@ -90,43 +92,31 @@ def main(stdscr):
             ui.display_title(stdscr)
             ui.display_controls(stdscr)
             stdscr.refresh()
+
             key = stdscr.getch()
-            if key == 24:
+            if key == KEY_EXIT:
                 break
-            elif key == 18:
+            elif key == KEY_START:
                 stdscr.clear()
                 text = text_generator.get_random_word()
 
                 curses.curs_set(True)
                 curses.echo()
-
                 user_typed = [] # Typed words (after space)
                 task_typed = [] # Original words, that user done
                 user_input = '' # Current typed word
-                is_correct = True
+                is_correct = True # Is correct current word
+                remaining = 0 # Time remaining
+                timer = 60 # Sec
 
-                #wpm = 0
-                #ch_accuracy = 0
-                #word_accuracy = 0
-                #words_incorrect = 0 # incorrect typed words counter
-                remaining = 0 # time remaining
-
-                timer = 60 # sec
-
-                # Before start
-                '''
-                ui.display_title(stdscr)
-                ui.display_task(stdscr, user_input, user_typed, text)
-                stdscr.addstr(2, width, str(timer))
-                stdscr.move(5, width)
-                stdscr.refresh()
-                user_input = chr(stdscr.getch())
-                is_correct = is_correct_input(user_input, text[0])
-                '''
+                # TODO: Type to start
 
                 start_time = time.time()
+
+                # Start time display thread
                 time_thread = threading.Thread(target=time_counter, args=(stdscr, start_time), daemon=True)
                 time_thread.start()
+
                 stdscr.refresh()
                 while time_thread.is_alive():
                     stdscr.clear()
@@ -136,7 +126,7 @@ def main(stdscr):
                     ui.display_task(stdscr, user_input, user_typed, task_typed, text)
                     ui.display_user_input(stdscr, user_input, is_correct)
                     stdscr.refresh()
-                    user_input = get_user_input(stdscr, user_input)
+                    user_input, text, task_typed = get_user_input(stdscr, user_input, user_typed, text, task_typed)
                     is_correct = is_correct_input(user_input, text[0])
 
                 key = stdscr.getch()
--- ui.py
+++ ui.py
@@ -7,18 +7,20 @@ x = width // 2 - len("Hello, world!") // 2
 y = height // 2
 stdscr.addstr(y, x, "Hello, world!")
 '''
-table_width = 55
+STATS_TABLE_W = 55 # Stats table width
+EXIT_SHORTCUT = "^X"
+START_SHORTCUT = "^R"
 
-def init_colorpairs(stdscr):
+def init_colorpairs(stdscr) -> None:
     # Color pairs. name, FG, BG
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE) # Menu title
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK) # Correct text
     curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK) # Incorrect text
 
-def get_window_size(stdscr):
+def get_window_size(stdscr) -> tuple:
     # Get window size
     global height, width
-    height, width = stdscr.getmaxyx()
+    height, width = stdscr.getmaxyx() # Screen width, height
     return height, width
 
 def display_title(stdscr):
@@ -30,56 +32,48 @@ def display_title(stdscr):
 # Bottom bar
 def display_controls(stdscr):
     # X Exit
-    stdscr.addstr(height-1, 0, "^X", curses.color_pair(1))
+    stdscr.addstr(height-1, 0, EXIT_SHORTCUT, curses.color_pair(1))
     stdscr.addstr(" Exit")
     # R Start
-    stdscr.addstr(height-1, 10, "^R", curses.color_pair(1))
+    stdscr.addstr(height-1, 10, START_SHORTCUT, curses.color_pair(1))
     stdscr.addstr(" Start")
 
 # wpm, c_accuracy, w_accuracy, w_incorrect, time
 def display_stats(stdscr, wpm:int, ch_acc:float, c_inc:int, w_acc:float, w_inc:int) -> None:
-    str_wpm = str(wpm).rjust(3)
-    str_ch_acc = str(ch_acc).rjust(5)
-    stdscr.addstr(7, width//2 - table_width//2, f"|                        wpm  {str_wpm}                     |", curses.A_BOLD)
-    stdscr.addstr(8, width//2 - table_width//2, "|-----------------------------------------------------|")
-    stdscr.addstr(9, width//2 - table_width//2, f"|   char accuracy | {str_ch_acc}% |    word accuracy| {str(w_acc).rjust(5)}% |")
-    stdscr.addstr(10, width//2 - table_width//2, f"| incorrect chars | {str(c_inc).rjust(5)}  | incorrect words | {str(w_inc).rjust(5)}  |")
-
-    # TODO: wpm
-    # TODO: acc
-    # TODO: time
-    pass
+    wpm = str(wpm).rjust(3)
+    ch_acc = str(ch_acc).rjust(5)
+    w_acc = str(w_acc).rjust(5)
+    c_inc = str(c_inc).rjust(5)
+    w_inc = str(w_inc).rjust(5)
+    # Table header
+    stats_header = f"|{' ' * (STATS_TABLE_W//2-4)}wpm {wpm}{' ' * (STATS_TABLE_W//2-4)}|"
+    stdscr.addstr(7, width//2 - STATS_TABLE_W//2, stats_header, curses.A_BOLD)
+    # Table body
+    stdscr.addstr(8, width//2 - STATS_TABLE_W//2, f"|{'-' * (STATS_TABLE_W-2)}|")
+    stdscr.addstr(9, width//2 - STATS_TABLE_W//2, f"|   char accuracy | {ch_acc}% |    word accuracy| {w_acc}% |")
+    stdscr.addstr(10, width//2 - STATS_TABLE_W//2, f"| incorrect chars | {c_inc}  | incorrect words | {w_inc}  |")
 
 def display_task(stdscr, user:str, user_typed:list[str], task_typed:list[str], task_to_type:list[str]) -> None:
     typed = " ".join(user_typed)
     to_type = " ".join(task_to_type)
+
     # Display dim typed text
     dim_length = (width // 2) - len(typed) - len(user) - 1
-    
     stdscr.addstr(4, max(0, dim_length), typed[max(0, -dim_length):], curses.A_DIM | curses.color_pair(2))
-    '''
-    cursor = dim_length
-    for i, v in enumerate(user_typed):
-        color = curses.color_pair(2) if v == task_typed[i] else curses.color_pair(3)
-        stdscr.addstr(4, max(0, cursor), typed[max(0, -cursor):], curses.A_DIM | color)
-        cursor += len(v) + 1
-    '''
 
     # Display text to type
     right = min(width, width//2+len(user)) # Right slice border
     stdscr.addstr(4, max(0, width // 2 - len(user)), to_type[:right])
-    pass
 
-def display_user_input(stdscr, user:str, correct:bool):
+def display_user_input(stdscr, user:str, correct:bool) -> None:
     # Set text color depends on the correctness
     color = curses.color_pair(3) if not correct else 0
     left = max(0, -((width // 2)-len(user))) # Left print slice start
     stdscr.addstr(5, max(0, (width // 2) - len(user)), user[left:], color)
     stdscr.move(5, width // 2)
 
-def display_time(stdscr, time:float):
+def display_time(stdscr, time:float) -> None:
     time = str(time).rjust(2)
     stdscr.addstr(2, (width // 2) - len(time)//2, time)
     stdscr.move(5, width // 2)
     stdscr.refresh()
-    pass
